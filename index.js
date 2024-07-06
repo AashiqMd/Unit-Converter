cd1aebc6915c8923cea5f60aab65ecaa075eebcf
Aashiq Mohamed Baig <aashiq.baig@gmail.com>
2024-07-06 11:55:25 -0400

    Revert "First Commit"
    This reverts commit d0e1408e987b4363cd68317fabca010e53fea3c7.

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
+let scoreHome = 0
+let scoreGuest = 0
 
-const lengthEle = document.getElementById("length-display-ele")
-const volumeEle = document.getElementById("volume-display-ele")
-const massEle = document.getElementById("mass-display-ele")
-const inputEle = document.getElementById("input-ele")
-const convertBtn = document.getElementById("convert-btn")
+let homeElement = document.getElementById("home")
+let guestElement = document.getElementById("guest")
 
-convertBtn.addEventListener("click", () => {
-    const feet = Math.round(inputEle.value*3.28084 * 1000)/1000
-    const meter = Math.round(inputEle.value/3.28084 * 1000)/1000
-    const liter = Math.round(inputEle.value/0.264 * 1000)/1000
-    const gallon = Math.round(inputEle.value*0.264 * 1000)/1000
-    const kilogram = Math.round(inputEle.value/2.204 * 1000)/1000
-    const pound = Math.round(inputEle.value*2.204 * 1000)/1000
+function updateh1() {
+    scoreHome += 1
+    homeElement.textContent = scoreHome
+    console.log(scoreHome)
+}
 
-    lengthEle.textContent = `${inputEle.value} meters = ${feet} feet | ${inputEle.value} feet = ${meter} meters`
-    volumeEle.textContent = `${inputEle.value} liter = ${gallon} gallon | ${inputEle.value} gallon = ${liter} liter`
-    massEle.textContent = `${inputEle.value} kilogram = ${pound} pound | ${inputEle.value} pound = ${kilogram} kilogram`
+function updateh2() {
+    scoreHome += 2
+    homeElement.textContent = scoreHome
+    console.log(scoreHome)
+}
 
-})
+function updateh3() {
+    scoreHome += 3
+    homeElement.textContent = scoreHome
+    console.log(scoreHome)
+}
+
+function updateg1() {
+    scoreGuest += 1
+    guestElement.textContent = scoreGuest
+    console.log(scoreGuest)
+}
+
+function updateg2() {
+    scoreGuest += 2
+    guestElement.textContent = scoreGuest
+    console.log(scoreGuest)
+}
+
+function updateg3() {
+    scoreGuest += 3
+    guestElement.textContent = scoreGuest
+    console.log(scoreGuest)
+}
+
+function newGame()
+{
+    scoreHome = 0
+    scoreGuest = 0
+    homeElement.textContent = scoreHome
+    guestElement.textContent = scoreGuest
+}
